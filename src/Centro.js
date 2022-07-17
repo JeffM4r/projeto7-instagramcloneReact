7b284c57d07ddbd5c74ae569132d83fdd596b44c
Jefferson Martins <jeffersonmartins.jef@gmail.com>
2022-07-16 23:01:44 -0300

    Commit com todos os requisitos completos

--- src/Centro.js
+++ src/Centro.js
@@ -1,11 +1,19 @@
+import React from "react";
 import Stories from "./Stories";
 import Posts from "./Posts";
 
 function Centro() {
+    const postContents = [{ nome: "meowed", foto: "assets/img/meowed.svg", conteudo: "assets/img/gato-telefone.svg", curtidaPor: "respondeai", curtidaPorFoto: "assets/img/respondeai.svg", curtidaNumero: "101.523" },
+    { nome: "barked", foto: "assets/img/barked.svg", conteudo: "assets/img/dog.svg", curtidaPor: "adorable_animals", curtidaPorFoto: "assets/img/adorable_animals.svg", curtidaNumero: "99.159" }]
+   
+    const [isActive, setIsActive] = React.useState(false);
+    const [isShown, setIsShown] = React.useState(true);
     return (
         <div class="esquerda">
             <Stories />
-            <Posts />
+            <div class="posts">
+                { postContents.map(post => <Posts nome={post.nome} foto={post.foto} conteudo={post.conteudo} curtidaPor={post.curtidaPor} curtidaPorFoto={post.curtidaPorFoto} curtidaNumero={post.curtidaNumero}/>)}
+            </div>
         </div>
     )
 }
--- src/Posts.js
+++ src/Posts.js
@@ -1,29 +1,50 @@
-function Post() {
-    const postContents = [{ nome: "meowed", foto: "assets/img/meowed.svg", conteudo: "assets/img/gato-telefone.svg", curtidaPor: "respondeai", curtidaPorFoto: "assets/img/respondeai.svg", curtidaNumero: "101.523" },
-    { nome: "barked", foto: "assets/img/barked.svg", conteudo: "assets/img/dog.svg", curtidaPor: "adorable_animals", curtidaPorFoto: "assets/img/adorable_animals.svg", curtidaNumero: "99.159" }]
+import React from 'react';
+
+function Post(props) {
+
+    const [isActive, setIsActive] = React.useState(false);
+    const [isShown, setIsShown] = React.useState(true);
+
+    const {nome, foto, conteudo, curtidaPor, curtidaPorFoto, curtidaNumero} = props
+
+    function clickIcon(){
+        setIsShown(false);
+    }
+    function clickIcon2(){
+        setIsShown(true);
+        setIsActive(false);
+    }
+    function clickImage(){
+        if(isShown){
+            setIsShown(false);
+            setIsActive(true);
+        };
+    }
 
 
     return (
-        postContents.map(postContent =>
-            <div class="post">
+            <div class={isActive ? "post liked" : "post"} >
                 <div class="topo">
                     <div class="usuario">
-                        <img src={postContent.foto} />
-                        {postContent.nome}
+                        <img src={foto} />
+                        {nome}
                     </div>
                     <div class="acoes">
                         <ion-icon name="ellipsis-horizontal"></ion-icon>
                     </div>
                 </div>
 
-                <div class="conteudo">
-                    <img src={postContent.conteudo} />
+                <div class="conteudo" onClick={()=>{clickImage()}}>
+                    <img src={conteudo} />
+                    <div class="instagram-heart">
+                    </div>
                 </div>
 
                 <div class="fundo">
                     <div class="acoes">
-                        <div>
-                            <ion-icon name="heart-outline"></ion-icon>
+                        <div class="icones">
+                            <ion-icon class="heart" style={{display: isShown ? 'flex' : 'none'}} onClick={()=>clickIcon()} name="heart-outline"></ion-icon>
+                            <ion-icon class="heartfilled" style={{display: isShown ? 'none' : 'flex'}} onClick={()=>clickIcon2()} name="heart"></ion-icon>
                             <ion-icon name="chatbubble-outline"></ion-icon>
                             <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
@@ -33,25 +54,15 @@ function Post() {
                     </div>
 
                     <div class="curtidas">
-                        <img src={postContent.curtidaPorFoto} />
+                        <img src={curtidaPorFoto} />
                         <div class="texto">
-                            Curtido por <strong>{postContent.curtidaPor}</strong> e <strong>outras {postContent.curtidaNumero} pessoas</strong>
+                            Curtido por <strong>{curtidaPor}</strong> e <strong>outras {curtidaNumero} pessoas</strong>
                         </div>
                     </div>
                 </div>
             </div>
         )
-    )
 }
 
-function Posts() {
-    return (
-        <div class="posts">
 
-            <Post />
-            
-        </div>
-    )
-}
-
-export default Posts;
+export default Post;
