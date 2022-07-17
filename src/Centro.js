060d4665158fc520a83f4514e479ab61f0092461
Jefferson Martins <jeffersonmartins.jef@gmail.com>
2022-07-16 23:07:54 -0300

    Removido 2 linhas de codigo que nao estava sendo usada

--- src/Centro.js
+++ src/Centro.js
@@ -6,8 +6,6 @@ function Centro() {
     const postContents = [{ nome: "meowed", foto: "assets/img/meowed.svg", conteudo: "assets/img/gato-telefone.svg", curtidaPor: "respondeai", curtidaPorFoto: "assets/img/respondeai.svg", curtidaNumero: "101.523" },
     { nome: "barked", foto: "assets/img/barked.svg", conteudo: "assets/img/dog.svg", curtidaPor: "adorable_animals", curtidaPorFoto: "assets/img/adorable_animals.svg", curtidaNumero: "99.159" }]
    
-    const [isActive, setIsActive] = React.useState(false);
-    const [isShown, setIsShown] = React.useState(true);
     return (
         <div class="esquerda">
             <Stories />
